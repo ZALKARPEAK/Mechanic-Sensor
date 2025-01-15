fb986d6f4934c9a76c37e70ac07c4a482a7125a8
Z <peak@gmail.com>
2025-01-15 08:34:35 +0300

    first commit

---
+++ src/main/java/com/xemyru/sensorweather/SensorWeatherApplication.java
@@ -0,0 +1,13 @@
+package com.xemyru.sensorweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorWeatherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SensorWeatherApplication.class, args);
+    }
+
+}
